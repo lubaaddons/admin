649498e6b5879b36eb35ed77fa7ebb9deda4b7cc
Florian Steiner <fs@adiwidjaja.com>
2016-08-01 16:07:31 +0200

    Initial commit

--- src/Admin.php
+++ src/Admin.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace Luba;
+
+class Admin
+{
+	protected $table;
+
+	protected $displayed = ['*'];
+
+	protected $hidden = [];
+
+	protected $config;
+
+	public function __construct($config = [])
+	{
+		$this->config = $config;
+	}
+}
